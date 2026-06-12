423627d7b73e66071d2f2a23c79198088c1dd5de
mhayes853 <matthewha853@gmail.com>
2026-06-12 15:27:54 -0700

    More scaffolding

--- Sources/CNeedleXGrammar/bridging.cc
+++ Sources/CNeedleXGrammar/bridging.cc
@@ -1,1 +1,6 @@
 #include "bridging.h"
+#include <xgrammar/tokenizer_info.h>
+#include <xgrammar/compiler.h>
+#include <xgrammar/matcher.h>
+
+using namespace xgrammar;
--- Sources/CNeedleXGrammar/include/bridging.h
+++ Sources/CNeedleXGrammar/include/bridging.h
@@ -1,4 +1,41 @@
 #ifndef __NEEDLE_XGRAMMAR_BRIDGING_H__
 #define __NEEDLE_XGRAMMAR_BRIDGING_H__
 
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void* needle_xgrammar_compiler_t;
+typedef void* needle_xgrammar_matcher_t;
+typedef void* needle_xgrammar_grammar_t;
+
+needle_xgrammar_compiler_t needle_xgrammar_compiler_init(const char** encoded_vocab, size_t vocab_size);
+void needle_xgrammar_compiler_set_memory_limit(needle_xgrammar_compiler_t compiler, int64_t limit);
+void needle_xgrammar_compiler_set_max_threads(needle_xgrammar_compiler_t compiler, int64_t threads);
+void needle_xgrammar_compiler_set_cache_enabled(needle_xgrammar_compiler_t compiler, int is_enabled);
+void needle_xgrammar_compiler_destroy(needle_xgrammar_compiler_t compiler);
+
+needle_xgrammar_matcher_t needle_xgrammar_compile_matcher(
+    needle_xgrammar_compiler_t compiler,
+    needle_xgrammar_grammar_t grammar
+);
+needle_xgrammar_matcher_t needle_xgrammar_matcher_fork(needle_xgrammar_matcher_t matcher);
+int needle_xgrammar_matcher_next_bitmask(needle_xgrammar_matcher_t matcher, int* bitmask);
+int needle_xgrammar_matcher_accept_token(needle_xgrammar_matcher_t matcher, int token);
+int needle_xgrammar_matcher_is_completed(needle_xgrammar_matcher_t matcher);
+int needle_xgrammar_matcher_is_terminated(needle_xgrammar_matcher_t matcher);
+void needle_xgrammar_matcher_rollback(needle_xgrammar_matcher_t matcher, int num_tokens);
+void needle_xgrammar_matcher_reset(needle_xgrammar_matcher_t matcher);
+void needle_xgrammar_matcher_destroy(needle_xgrammar_matcher_t matcher);
+
+needle_xgrammar_grammar_t needle_xgrammar_grammar_init(const char* tools_json);
+void needle_xgrammar_grammar_destroy(needle_xgrammar_grammar_t grammar);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
