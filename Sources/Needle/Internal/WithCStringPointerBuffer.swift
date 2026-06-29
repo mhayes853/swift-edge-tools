d03a3ce03c19bf4addd31523b479966352568a7e
mhayes853 <matthewha853@gmail.com>
2026-06-29 12:56:40 -0700

    Update C String Copying

--- Sources/Needle/Internal/WithCStringPointerBuffer.swift
+++ Sources/Needle/Internal/WithCStringPointerBuffer.swift
@@ -2,11 +2,15 @@ func withCopiedCStringPointerBuffer<Result>(
   _ strings: [String],
   _ body: (UnsafeMutableBufferPointer<UnsafePointer<CChar>?>) throws -> Result
 ) rethrows -> Result {
-  let mutablePointers = strings.map { strdup($0) }
+  var mutablePointers = strings.map { UnsafePointer<CChar>(strdup($0.utf8CString)) }
   defer { mutablePointers.forEach { $0?.deallocate() } }
+  return try mutablePointers.withUnsafeMutableBufferPointer { try body($0) }
+}
 
-  var constPointers = mutablePointers.map { UnsafePointer<CChar>($0) }
-  return try constPointers.withUnsafeMutableBufferPointer { buffer in
-    try body(buffer)
+private func strdup(_ s: ContiguousArray<CChar>) -> UnsafeMutablePointer<CChar>? {
+  let dst = UnsafeMutablePointer<CChar>.allocate(capacity: s.count)
+  for i in 0..<s.count {
+    dst[i] = s[i]
   }
+  return dst
 }
