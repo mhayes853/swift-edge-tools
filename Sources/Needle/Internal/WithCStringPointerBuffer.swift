d044dea53b667ec08928303950c65fc6a23b1e57
mhayes853 <matthewha853@gmail.com>
2026-06-29 13:01:15 -0700

    CI Workflow timeout

--- Sources/Needle/Internal/WithCStringPointerBuffer.swift
+++ Sources/Needle/Internal/WithCStringPointerBuffer.swift
@@ -2,15 +2,13 @@ func withCopiedCStringPointerBuffer<Result>(
   _ strings: [String],
   _ body: (UnsafeMutableBufferPointer<UnsafePointer<CChar>?>) throws -> Result
 ) rethrows -> Result {
-  var mutablePointers = strings.map { UnsafePointer<CChar>(strdup($0.utf8CString)) }
+  var mutablePointers = strings.map {
+    let dst = UnsafeMutablePointer<CChar>.allocate(capacity: $0.utf8CString.count)
+    for i in 0..<$0.utf8CString.count {
+      dst[i] = $0.utf8CString[i]
+    }
+    return UnsafePointer<CChar>(dst) as UnsafePointer<CChar>?
+  }
   defer { mutablePointers.forEach { $0?.deallocate() } }
   return try mutablePointers.withUnsafeMutableBufferPointer { try body($0) }
-}
-
-private func strdup(_ s: ContiguousArray<CChar>) -> UnsafeMutablePointer<CChar>? {
-  let dst = UnsafeMutablePointer<CChar>.allocate(capacity: s.count)
-  for i in 0..<s.count {
-    dst[i] = s[i]
-  }
-  return dst
 }
